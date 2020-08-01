42a8ab48a20b0bf86009b793277f396456f2a841
VishnuSD <vishnu@smarterdharma.com>
2020-08-01 17:52:06 +0530

    git ignore editted & build

---
+++ build/precache-manifest.db0717e86786b9b36f10cfa3712c3737.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a71e24a18c01799659ce488d420181c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48692bddf88d4af6d1e3",
+    "url": "/static/css/main.c26464eb.chunk.css"
+  },
+  {
+    "revision": "dc93d2c26568bd4ac555",
+    "url": "/static/js/2.eb6061a7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.eb6061a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48692bddf88d4af6d1e3",
+    "url": "/static/js/main.f5730cdd.chunk.js"
+  },
+  {
+    "revision": "f4dc032434d64ed692aa",
+    "url": "/static/js/runtime-main.ef23f142.js"
+  }
+]);
